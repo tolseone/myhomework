94c7e05376a015585a6313f2275083d93ba2e218
tolseone <asemisynov@mail.ru>
2023-02-19 14:03:16 +0300

    внесли правки в ex_02

--- ex_02/Program.cs
+++ ex_02/Program.cs
@@ -3,9 +3,9 @@ int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if(a > b)
 {
-    Console.Write("max = {0} min = {1}", a, b);
+    Console.Write($"max = {a}, min = {b}");
 }
 else
 {
-    Console.Write("max = {0} min = {1}", b, a);
+    Console.Write($"max = {b}, min = {a}");
 }
