dd5db04b3fd5895aa075cfe8957468d58b5e50d7
tolseone <asemisynov@mail.ru>
2023-02-19 13:45:48 +0300

    add ex_1

---
+++ ex_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа на отдельных строчках каждое: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == Math.Pow(b, 2))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
