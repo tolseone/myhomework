e3185ca4900938962c272c75a0ea0334be7ddd94
tolseone <asemisynov@mail.ru>
2023-02-19 13:53:21 +0300

    add ex_02

---
+++ ex_02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите 2 числа каждое на отдельной строке: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.Write("max = {0} min = {1}", a, b);
+}
+else
+{
+    Console.Write("max = {0} min = {1}", b, a);
+}
