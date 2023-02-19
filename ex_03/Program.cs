7d3c6513e33f87dfdb4106d7ea93d008800304ae
tolseone <asemisynov@mail.ru>
2023-02-19 14:01:16 +0300

    add ex_03

---
+++ ex_03/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Напишите цифру, а я вам выдам день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num == 1)
+{
+    Console.Write("Понедельник");
+}
+else if(num == 2)
+{
+    Console.Write("Вторник");
+}
+else if(num == 3)
+{
+    Console.Write("Среда");
+}
+else if(num == 4)
+{
+    Console.Write("Четверг");
+}
+else if(num == 5)
+{
+    Console.Write("Пятница");
+}
+else if(num == 6)
+{
+    Console.Write("Суббота");
+}
+else if(num == 7)
+{
+    Console.Write("Воскресенье");
+}
+else
+{
+    Console.Write("Пашёл нахуй");
+}
