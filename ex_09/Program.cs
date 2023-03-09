5611c1ce07b155605383d261bcf75aba8da5b6d5
tolseone <asemisynov@mail.ru>
2023-03-09 20:59:18 +0300

    Сделал упражнение 9

---
+++ ex_09/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random().Next(10,100000);
+int max_d = -1;
+for (int i = a; i > 0; i /= 10)
+{
+    if (i % 10 > max_d)
+    {
+    max_d = i % 10;
+    }
+}
+Console.WriteLine("Нарандомилось число: " + a);
+Console.WriteLine("Наибольшая цифра: " + max_d);
