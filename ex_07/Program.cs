373f5321f497a1949ec77b1bb57809b72542a0dc
tolseone <asemisynov@mail.ru>
2023-03-09 20:41:43 +0300

    Сделал упражнение 7

---
+++ ex_07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое 3-ёх значное число, а я выведу его последнюю цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+if ( n > 99 && n < 1000 )
+{
+    int d = n % 10;
+    Console.WriteLine("Последняя цифра: " + d);
+}
+else
+{
+    Console.WriteLine("Ты за меня кого держишь?)");
+}
