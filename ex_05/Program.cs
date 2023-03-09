d5ea900a3fbdfa9a0252cb8d83f344a729742f7d
tolseone <asemisynov@mail.ru>
2023-03-09 19:21:36 +0300

    Сделал 5 упражнение на вывод последовательности, но нужно доделать по хорошему

---
+++ ex_05/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите Число, а я вам выведу все целые числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int num = a;
+if (a > 0)
+{
+    while (num >= -a)
+    {
+        Console.Write(num);
+        Console.Write(" ");
+        num -= 1;
+    }
+}
+else
+{
+    if (a < 0)
+    {
+        while (num <= -a)
+        {
+            Console.Write(num);
+            Console.Write(" ");
+            num += 1;
+        }
+    }
+    else
+    {
+        Console.Write("0");
+    }
+
+}
