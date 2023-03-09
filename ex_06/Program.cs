2cba0557a86d324c2e460931ac67dc0613bdef43
tolseone <asemisynov@mail.ru>
2023-03-09 20:36:16 +0300

    Решил 6 упражнение

---
+++ ex_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число, а я проверю, делится ли оно на 2 без остатка или нет");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Конечно же делится и получится: " + a / 2);
+}
+else
+{
+    Console.Write("Ohh NO!");
+}
