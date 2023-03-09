63ff4dbec4452f0fcba19b7721bbdb69482db18f
tolseone <asemisynov@mail.ru>
2023-03-09 20:29:48 +0300

    Завершил ex_05 правильно

--- ex_05/Program.cs
+++ ex_05/Program.cs
@@ -1,29 +1,49 @@
-﻿Console.WriteLine("Введите Число, а я вам выведу все целые числа");
+﻿// Console.WriteLine("Введите Число, а я вам выведу все целые числа");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int num = a;
+// if (a > 0)
+// {
+//     while (num >= -a)
+//     {
+//         Console.Write(num);
+//         Console.Write(", ");
+//         num -= 1;
+//     }
+// }
+// else
+// {
+//     if (a < 0)
+//     {
+//         while (num <= -a)
+//         {
+//             Console.Write(num);
+//             Console.Write(", ");
+//             num += 1;
+//         }
+//     }
+//     else
+//     {
+//         Console.Write("0");
+//     }
+
+// }
+Console.WriteLine("Введите число, а я вам выведу все целые числа");
 int a = Convert.ToInt32(Console.ReadLine());
-int num = a;
 if (a > 0)
 {
-    while (num >= -a)
+    for (int i = -a; i <= a; i++ )
     {
-        Console.Write(num);
-        Console.Write(", ");
-        num -= 1;
+        Console.Write(i + ", ");
+    }
+}
+else if (a < 0)
+{
+    for (int i = a; i <= -a; i++ )
+    {
+        Console.Write(i + ", ");
     }
 }
 else
 {
-    if (a < 0)
-    {
-        while (num <= -a)
-        {
-            Console.Write(num);
-            Console.Write(", ");
-            num += 1;
-        }
-    }
-    else
-    {
-        Console.Write("0");
-    }
-
+    Console.Write("0");
 }
