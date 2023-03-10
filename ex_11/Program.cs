cf22ef21e57a7c832feee0cbde52ef72b0ac66e3
tolseone <asemisynov@mail.ru>
2023-03-10 12:17:07 +0300

    вывод первой и последней цифры 3-ёх значного числа

---
+++ ex_11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int new_number = number / 100 * 10  + number % 10;
+
+Console.WriteLine($"num: {number}, new_number {new_number}");
