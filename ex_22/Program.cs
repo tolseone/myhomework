bb01822db309b36eddd1b1568e3df6645b01ce0c
tolseone <asemisynov@mail.ru>
2023-03-10 17:17:34 +0300

    цикл for, библиотека math

---
+++ ex_22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int num = ReadNum("Введите число");
+for( int i = 1; i <= num; i++)
+{
+    double sq = Math.Pow( i, 2);
+    Console.Write($"{sq}, ");
+}
