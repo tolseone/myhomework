f4fdbc524db38efdc9ff05f55e9b3ee37e7f4c42
tolseone <asemisynov@mail.ru>
2023-03-11 15:45:41 +0300

    упражнение на вывод кубов

---
+++ ex_23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int num = ReadNum("Введите число");
+for( int i = 1; i < num; i++)
+{
+    double sq = Math.Pow( i, 3);
+    Console.Write($"{sq}, ");
+}
+Console.WriteLine($"{Math.Pow(num, 3)}.");
