d91b64a2f692ed0dea6a1c5fd91abdc66e5251bc
tolseone <asemisynov@mail.ru>
2023-03-12 20:22:00 +0300

    применение функции суммирования через цикл for

---
+++ ex_24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму
+// чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadNumber(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Sumfrom1toNumber(int number)
+{
+    int sum = 0;
+    for ( int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine(Sumfrom1toNumber(ReadNumber("Напишите число")));
