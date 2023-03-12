363249fddc15683165fe30a523bf9ad8951406b0
tolseone <asemisynov@mail.ru>
2023-03-12 20:36:49 +0300

    подсчёт количества цифр в числе через функцию с циклом for

---
+++ ex_26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число и выдаёт количество
+// цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int quantity_digits(int number)
+{
+    int quantity = 0;
+    for (int i = number; i != 0; i /= 10)
+    {
+        quantity += 1;
+    }
+    return quantity;
+}
+
+Console.WriteLine(quantity_digits(ReadNum("Введите число, а я посчитаю количество цифр в нём")));
