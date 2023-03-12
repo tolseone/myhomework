2ad79f78bf70862d972eb8026a45797d82287fcd
tolseone <asemisynov@mail.ru>
2023-03-12 20:44:21 +0300

    произведение чисел от 1 до N через функцию с циклом for

---
+++ ex_28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу,
+// которая принимает на вход число N и
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int Prod(int number)
+{
+    int prod = 1;
+    for (int i = 1; i <= number; i++)
+    {
+    prod *= i;
+    }
+    return prod;
+}
+
+Console.WriteLine(Prod(ReadNum("напишите число, а я выведу Вам произведение чисел от 1 до него")));
