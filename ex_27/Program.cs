f350f003a73adfe8cb23d3a19260e169b9d958b3
tolseone <asemisynov@mail.ru>
2023-03-12 20:40:35 +0300

    Сумма цифр в числе через цикл for

---
+++ ex_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int sum_digits(int number)
+{
+    int sum_dgts = 0;
+    for (int i = number; i != 0; i /= 10)
+    {
+        int last_d = i % 10;
+        sum_dgts += last_d;
+    }
+    return sum_dgts;
+}
+
+Console.WriteLine(sum_digits(ReadNum("Введите число, а я посчитаю сумму цифр в нём")));
