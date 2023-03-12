162e7bbe44e00f7f499626cc1fdf942df4eca605
tolseone <asemisynov@mail.ru>
2023-03-12 20:31:21 +0300

    возведение в степень через функцию

---
+++ ex_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadNumber(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double Finally(double num, int stepen)
+{
+    double res = Math.Pow(num, stepen);
+    return res;
+}
+
+double a = Convert.ToDouble(ReadNumber("Введите число А"));
+int b = ReadNumber("Введите Число B");
+
+Console.WriteLine(Finally(a, b));
