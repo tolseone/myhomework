b3b6d85256c46e74ebac015796fe8f5216a4dbef
tolseone <asemisynov@mail.ru>
2023-03-12 22:22:14 +0300

    задалопредленный массив через функции

---
+++ ex_30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов,
+// заполненный нулями и единицами в
+// случайном порядке.
+// [1,0,1,1,0,1,0,0]
+int [] GenerateNewArray()
+{
+    int [] my_array = new int [8];
+    Random random = new Random();
+    for (int i = 0; i < 8; i++)
+    {
+        my_array[i] = random.Next(0, 2);
+    }
+    return my_array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+
+int[] my_arr = GenerateNewArray();
+PrintArray(my_arr);
