8070a17df4632857935d254679df8829b00ca2fb
tolseone <asemisynov@mail.ru>
2023-03-12 22:18:35 +0300

    задал рандом массив

---
+++ ex_29/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int [] GenerateNewArray(int len, int minValue, int maxvalue)
+{
+    int [] my_array = new int [len];
+    Random random = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        my_array[i] = random.Next(minValue, maxvalue + 1);
+    }
+    return my_array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+int quantity = ReadNum("Введите количество элементов в массиве");
+int min = ReadNum("нижнее значение элементов");
+int max = ReadNum("верхнее значение элементов");
+int[] my_arr = GenerateNewArray(quantity, min, max);
+PrintArray(my_arr);
