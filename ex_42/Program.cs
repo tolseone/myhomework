8c3515b2fb9b950571644ab9064f3531d9939ddf
tolseone <asemisynov@mail.ru>
2023-03-15 13:53:12 +0300

    Перевод десятичного числа в двоичное

---
+++ ex_42/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+string DOUBLENUMBER(int n)
+{
+    string text = string.Empty;
+    for (int i = n; i > 0; i /= 2)
+    {
+        int d = i % 2;
+        text = d + text;
+    }
+    return text;
+}
+
+int number = ReadNum("Введите число, а я переведу его в двоичное: ");
+string result = DOUBLENUMBER(number);
+Console.WriteLine(result);
