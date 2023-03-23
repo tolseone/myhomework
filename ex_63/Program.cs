930dc8818ca95e2c1b5a570cab174737e63b7dd2
tolseone <asemisynov@mail.ru>
2023-03-23 15:44:28 +0300

    рекурсия метода от 1 до n

---
+++ ex_63/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int ReadNum(string msgToUser)
+{
+    Console.WriteLine(msgToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void from_one_to_N(int ReadNum)
+{
+if (ReadNum < 1) return;
+from_one_to_N(ReadNum - 1);
+Console.Write($"{ReadNum} ");
+}
+
+from_one_to_N(ReadNum("Введите число"));
